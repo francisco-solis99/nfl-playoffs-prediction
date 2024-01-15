45db797e03635d79341a99b2897ce99ad3bf94f0
francisco-solis99 <franciscosoliscss@gmail.com>
2024-01-15 11:54:35 -0600

    chore: :art: add the match card and brackets matches components

--- src/data/utils/matches.ts
+++ src/data/utils/matches.ts
@@ -1,14 +1,43 @@
-import type { MatchesByRound, Round, RoundType } from "@/data/types/matches.types"
+import type { Match, MatchesByRound, Round, RoundTypeSlug } from "@/data/types/matches.types"
 import allMatches from '@/data/matches.json'
 
 const matchesData = allMatches as MatchesByRound
 
-export function getMatchesByRound({ roundName, conference }: { roundName: RoundType, conference: "AFC" | "NFC" }) {
-  const matchesByRound: Round | undefined = matchesData.find(item => item.round === roundName)
+export function getMatchesByRoundConference({ roundName, conference }: { roundName: RoundTypeSlug, conference: "AFC" | "NFC" }) {
+  const matchesByRound: Round | undefined = getRoundInfo({ roundName })
 
   if (!matchesByRound) {
-    throw new Error('Your round name has to be in the RoundType options')
+    throw new Error(`Your round name has to be in the RoundTypeSlug options cause ${roundName}`)
   }
   const matchesByConference = matchesByRound.matches[conference]
   return matchesByConference
+}
+
+export function getMatchById({ roundName, id }: { roundName: RoundTypeSlug, id: number }) {
+  const infoByRound: Round | undefined = getRoundInfo({ roundName })
+  if (!infoByRound) {
+    throw new Error('Your round name has to be in the RoundTypeSlug options')
+  }
+
+  type MatchesConference = ['AFC' | 'NFC', Match[]]
+
+
+  const allMatches = Object.entries(infoByRound.matches)
+
+  for(const [conference, matches] of allMatches) {
+
+    const matchFound = matches.find(match => match.id === id)
+    if(matchFound) {
+      return {
+        match: matchFound,
+        conference
+      }
+    }
+  }
+
+  return null;
+}
+
+export function getRoundInfo({ roundName }: { roundName: RoundTypeSlug }) {
+  return matchesData.find(item => item.slug === roundName)
 }
